79bcb5661eab30e64c47690960a039ad9b074f6b
lk_kirill <lukyanchuk.kiya@gmail.com>
2022-09-27 19:40:52 +0600

    .

--- src/Option_5/SumAlgorithm.java
+++ src/Option_5/SumAlgorithm.java
@@ -1,4 +1,17 @@
 package Option_5;
 
+import java.util.Scanner;
+
 public class SumAlgorithm {
+    public static int sumOfNumber(int n) {
+        if (n <= 1)
+            return n;
+        return n + sumOfNumber(n - 1);
+    }
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(sumOfNumber(n));
+    }
 }
+
